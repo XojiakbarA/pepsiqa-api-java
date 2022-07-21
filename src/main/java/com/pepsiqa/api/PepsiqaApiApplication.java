56b19bd0f53fcedaa8e6daa806931e790977ea42
Xojiakbar <87039534+XojiakbarA@users.noreply.github.com>
2022-07-21 15:58:14 +0500

    init commit

---
+++ src/main/java/com/pepsiqa/api/PepsiqaApiApplication.java
@@ -0,0 +1,13 @@
+package com.pepsiqa.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PepsiqaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PepsiqaApiApplication.class, args);
+    }
+
+}
